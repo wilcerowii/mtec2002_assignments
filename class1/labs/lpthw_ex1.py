66a460f72da68125b5fb678190671e4412cca573
wilcerowii <wilzodiacnights@hotmail.com>
2012-02-01 19:09:50 -0500

    my first assignments

---
+++ class1/labs/lpthw_ex1.py
@@ -0,0 +1,7 @@
+print print "Hello world!"
+print print "Hello Again"
+print print "I like typing this."
+print print "This is fun."
+print print 'Yay! Printing.'
+print print "I'd much rather you 'not'."
+print print 'I "said" do not touch this.'

484a8ace69c3ebe8a8ca2bed0e91045f80177e43
wilmer <student@ENT-V321-009.local>
2012-02-29 18:59:25 -0500

    adding files for lab 2

---
+++ class5/lists_loops_review.py
@@ -0,0 +1,16 @@
+"""
+loops_lists_review.py
+
+1. Create a list of numbers
+2. Append a number to that list of numbers
+3. Get the length of the list
+4. Loop through the list and return the number minus 1
+"""
+
+a = range(10)
+a.append(12)
+b = len(a)
+print b
+print len (a)
+fro c in a:
+	print c -l

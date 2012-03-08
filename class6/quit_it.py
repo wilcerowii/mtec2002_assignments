7c3c75bec8192d9a184ebdc9f9909c3f63b4f9ed
Student <student@ENT-V321-009.local>
2012-03-07 19:27:38 -0500

    adding files for class

---
+++ class6/quit_it.py
@@ -0,0 +1,31 @@
+"""
+quit_it.py
+===
+1. Ask the user to type quit if they want to leave the program - "Type quit 
+to stop this program"
+2. Prompt the user for input, and store the input in a variable named command
+3. Use a while loop to continue to ask for input until the user types in quit
+
+Example Output:
+Type quit to stop this program
+>I don't want it to stop yet
+Type quit to stop this program
+>Still ok
+Type quit to stop this program
+>quit
+Thanks!  See you later.
+"""
+
+
+def type quit to stop this program
+response raw_input == "">" ):
+if response "i don't want it to stop yet"
+	print "type quit to stop this program"
+	response raw in_put == "">" ):
+if response "still ok"
+	print "type quit to stop this program"
+if response raw_input == "">" ):
+	
+else 
+if response "quit"
+	print "thanks! see you later."

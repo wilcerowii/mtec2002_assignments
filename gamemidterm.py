80ae780a8067f19b0c7c048ef0e21de6e7b42ef3
wilcero <wilzodiacnights@hotmail.com>
2012-03-28 18:20:17 -0400

    midterm

---
+++ gamemidterm.py
@@ -0,0 +1,61 @@
+import random
+Name = raw_input ("Enter Name here: ") 
+print "Hello " + Name + "! I am a python script specially designed for a game called rock paper scissors!"
+answer = raw_input ("Do you want to play? : ")
+if answer == ("yes"):
+    print "Okay! Lets play!"
+    PC = raw_input ("Choose rock,paper or scissors: ")
+    rand = random.randint(1, 3) #1 means rock	  
+
+    if PC == ("rock"):
+        if rand == (1):
+            print "Tie game!"
+        elif rand == 2:
+            print "Game lost!"
+        elif rand == 3:
+            print "Game Win!"
+    if PC == ("paper"):
+        if rand == 2:
+            print "Tie game!"
+        elif rand == 3:
+            print "Game lost!"
+        elif rand == 1:
+            print "Game Win!"
+    if PC == ("scissors"):
+        if rand == 3:
+            print "Tie game!"
+        elif rand == 1:
+            print "Game lost!"
+        elif rand == 2:
+            print "Game Win!"
+else :
+    print "aww!"
+"""else (quit)
+
+
+
+Rock, paper, scissors
+
+    _______
+---    ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+
+    _______
+---    ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+
+    _______
+---    ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+
